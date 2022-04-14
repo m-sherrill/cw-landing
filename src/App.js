a2ceffbc0819309841c5fa60e920f3ea2a2e94d3
Morgan Sherrill <mo.sherrill@outlook.com>
2022-04-13 23:21:29 -0700

    Started working on the header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
         
         <p>
           CARTRIDGE WORLD LANDING!!!
         </p>
       
-      </header>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import { createTheme } from '@mui/material/styles'
+
+
+const theme = createTheme(theme => ({
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
+  
 
 const Header = () => {
-  return (
-    <div>Header</div>
+    const [open, setOpen] = useState(false);
+  
+    const handleOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
+    return (
+      <AppBar position="static" theme={theme}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+            
+          </IconButton>
+          <Typography variant="h6">
+            Title
+          </Typography>
+        </Toolbar>
+      
+      </AppBar>
   )
 }
 
