5fb403fbedf21674dae0bff62d675b38911da5e3
Morgan Sherrill <mo.sherrill@outlook.com>
2022-06-28 22:57:06 -0700

    creating the foundation skeleton of the landing page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router,
+Routes, 
+Route } from 'react-router-dom'
 import './App.css';
 import Header from './components/Header'
+import Footer from './components/Footer'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Order from './pages/Order'
+import Policy from './pages/Policy'
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
-      <Header />
-        
-        <p>
-          CARTRIDGE WORLD LANDING!!!
-        </p>
+<Router>
+  <Header />
+  <div className="content-wrapper">
+        <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/order" element={<Order />} />
+                <Route path="/policy" element={<Policy />} />
+          </Routes>
+      </div>
+      <Footer />
+        </Router>
       
     </div>
   );
